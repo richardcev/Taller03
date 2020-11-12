f9297a7ed1bc1d5f0b87413cb49abd3566fe80fe
cevavid <cevavid@outlook.es>
2020-11-11 22:45:53 -0500

    Clase padre Postre

--- TallerSOLID/src/Postres/Helado.java
+++ TallerSOLID/src/Postres/Helado.java
@@ -12,36 +12,24 @@ import Adicionales.Aderezo;
  *
  * @author Pedro Mendoza
  */
-public class Helado{
+public class Helado extends Postre{
     private String sabor;
-    private double precioParcial;
-    private ArrayList<Aderezo> aderezos;
+
     
     public Helado(String sabor){
-        aderezos= new ArrayList<>();
-        this.sabor=sabor;
+    	super(sabor);
         this.precioParcial = 7.85;
     }
     
-    public double calcularPrecioFinal(){
-        double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
-        return precioFinal;
-    }
 
-    public ArrayList<Aderezo> getAderezos() {
-        return aderezos;
-    }
+
 
     @Override
     public String toString() {
         return "Helado{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
     }
     
-    public String showPrecioFinal(){
-        return "Precio Final: $ " + calcularPrecioFinal();
-    }
-    
+
     
     
 }
---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,34 @@
+package Postres;
+
+import java.util.ArrayList;
+
+import Adicionales.Aderezo;
+
+public class Postre {
+    protected String sabor;
+    protected double precioParcial;
+    protected ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor){
+        aderezos= new ArrayList<>();
+        this.sabor=sabor;
+    }
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+    
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+    
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+
+    
+    
+
+}
